72058718cf5c79681676bdf70a31dff7b18cbdc9
David Winslow <the@machini.st>
2015-09-24 13:09:24 -0700

    fixed item counts in getDatatable()

--- app/controllers/admin/CategoriesController.php
+++ app/controllers/admin/CategoriesController.php
@@ -233,7 +233,24 @@ class CategoriesController extends AdminController
             return ucwords($categories->category_type);
         })
         ->addColumn('count', function($categories) {
-            return ($categories->category_type=='asset') ? link_to('/admin/settings/categories/'.$categories->id.'/view', $categories->assetscount()) : $categories->toolscount();
+			// this could be 
+			// $categories->assetscount() + $categories->toolscount() + $categories->consumablescount() + ...
+			$count = 0;
+			switch ($categories->category_type){
+				case 'asset':
+					$count = $categories->assetscount();
+					break;
+				case 'tool':
+					$count = $categories->toolscount();
+					break;
+				case 'consumable':
+					$count = $categories->consumablescount();
+					break;
+				case 'gage':
+					
+					break;
+			}
+            return $count; 
         })
         ->addColumn('acceptance', function($categories) {
             return ($categories->require_acceptance=='1') ? '<i class="fa fa-check" style="margin-right:50%;margin-left:50%;"></i>' : '';
