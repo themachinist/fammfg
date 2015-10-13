77f03213ecd36d7bf4ec505f43011a7357fbce5a
David Winslow <the@machini.st>
2015-10-13 07:36:05 -0700

    removed needs_maintenance column

--- app/views/backend/fixtures/index.blade.php
+++ app/views/backend/fixtures/index.blade.php
@@ -18,39 +18,38 @@
 
 <div class="row form-wrapper">
     {{ Datatable::table()
-                ->addColumn(Lang::get('admin/fixtures/table.title'),
-                            Lang::get('admin/fixtures/table.serial'),
-                            Lang::get('admin/fixtures/form.copies'),
-                            Lang::get('admin/fixtures/form.available_copies'),
-                            Lang::get('admin/fixtures/table.needs_maintenance'),
-                            Lang::get('admin/fixtures/form.notes'),
-                            Lang::get('table.actions'))
-                ->setOptions(
-                        array(
-	                        'language' => array(
-			            	'search' => Lang::get('general.search'),
-			            	'lengthMenu' => Lang::get('general.page_menu'),
-			            	'loadingRecords' => Lang::get('general.loading'),
-			            	'zeroRecords' => Lang::get('general.no_results'),
-			            	'info' => Lang::get('general.pagination_info'),
-			            	'processing' => Lang::get('general.processing'),
-			            	'paginate'=> array(
-			            		'first'=>Lang::get('general.first'),
-			            		'previous'=>Lang::get('general.previous'),
-			            		'next'=>Lang::get('general.next'),
-			            		'last'=>Lang::get('general.last'),
-			            		),
-			            	),
-                            'sAjaxSource'=>route('api.fixtures.list'),
-                            'dom' =>'CT<"clear">lfrtip',
-                            'colVis'=> array('showAll'=>'Show All','restore'=>'Restore','exclude'=>array(6),'activate'=>'mouseover'),
-                            'columnDefs'=> array(
-                                array('bSortable'=>false,'targets'=>array(6)),
-                                array('width'=>'20%','targets'=>array(6)),
-                                ),
-                            'order'=>array(array(0,'asc')),
-                        )
-                    )
-                ->render() }}
+->addColumn(Lang::get('admin/fixtures/table.title'),
+			Lang::get('admin/fixtures/table.serial'),
+			Lang::get('admin/fixtures/form.copies'),
+			Lang::get('admin/fixtures/form.available_copies'),
+			Lang::get('admin/fixtures/form.notes'),
+			Lang::get('table.actions'))
+->setOptions(
+	array(
+		'language' => array(
+			'search' => Lang::get('general.search'),
+			'lengthMenu' => Lang::get('general.page_menu'),
+			'loadingRecords' => Lang::get('general.loading'),
+			'zeroRecords' => Lang::get('general.no_results'),
+			'info' => Lang::get('general.pagination_info'),
+			'processing' => Lang::get('general.processing'),
+			'paginate'=> array(
+				'first'=>Lang::get('general.first'),
+				'previous'=>Lang::get('general.previous'),
+				'next'=>Lang::get('general.next'),
+				'last'=>Lang::get('general.last'),
+			),
+		),
+		'sAjaxSource'=>route('api.fixtures.list'),
+		'dom' =>'CT<"clear">lfrtip',
+		'colVis'=> array('showAll'=>'Show All','restore'=>'Restore','exclude'=>array(6),'activate'=>'mouseover'),
+		'columnDefs'=> array(
+			array('bSortable'=>false,'targets'=>array(5)),
+			array('width'=>'20%','targets'=>array(5)),
+		),
+		'order'=>array(array(0,'asc')),
+	)
+)
+->render() }}
 </div>
 @stop
