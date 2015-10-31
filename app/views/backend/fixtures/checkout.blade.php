6bfefd1b28b6a53b2b4b1eb0cba2b106aebc8829
David Winslow <the@machini.st>
2015-10-31 08:02:31 -0700

    fixed indents

--- app/views/backend/fixtures/checkout.blade.php
+++ app/views/backend/fixtures/checkout.blade.php
@@ -20,81 +20,75 @@
 </div>
 
 <div class="row form-wrapper">
-<!-- left column -->
-<div class="col-md-10 column">
+	<!-- left column -->
+	<div class="col-md-10 column">
 
-<form class="form-horizontal" method="post" action="" autocomplete="off">
-    <!-- CSRF Token -->
-    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+		<form class="form-horizontal" method="post" action="" autocomplete="off">
+			<!-- CSRF Token -->
+			<input type="hidden" name="_token" value="{{ csrf_token() }}" />
+			<!-- Asset name -->
+			<div class="form-group">
+			<label class="col-sm-2 control-label">@lang('admin/hardware/form.name')</label>
+				<div class="col-md-6">
+				  <p class="form-control-static">{{{ $fixtureseat->fixture->name }}}</p>
+				</div>
+			</div>
 
-            <!-- Asset name -->
-            <div class="form-group">
-            <label class="col-sm-2 control-label">@lang('admin/hardware/form.name')</label>
-                <div class="col-md-6">
-                  <p class="form-control-static">{{{ $fixtureseat->fixture->name }}}</p>
-                </div>
-            </div>
-
-            <!-- Serial -->
-            <div class="form-group">
-            <label class="col-sm-2 control-label">@lang('admin/hardware/form.serial')</label>
-                <div class="col-md-10">
-                  <p class="form-control-static" style="word-wrap: break-word;">{{{ $fixtureseat->fixture->serial }}}</p>
-                </div>
-            </div>
+			<!-- Serial -->
+			<div class="form-group">
+			<label class="col-sm-2 control-label">@lang('admin/hardware/form.serial')</label>
+				<div class="col-md-10">
+				  <p class="form-control-static" style="word-wrap: break-word;">{{{ $fixtureseat->fixture->serial }}}</p>
+				</div>
+			</div>
 
 			<!-- Asset -->
-            <div class="form-group {{ $errors->has('asset_id') ? ' has-error' : '' }}">
-                <label for="asset_id" class="col-md-2 control-label">@lang('admin/fixtures/form.asset')
-                 </label>
+			<div class="form-group {{ $errors->has('asset_id') ? ' has-error' : '' }}">
+				<label for="asset_id" class="col-md-2 control-label">@lang('admin/fixtures/form.asset')
+				 </label>
 
-                <div class="col-md-10">
-                    {{ Form::select('asset_id', $asset_list , Input::old('asset_id', $fixtureseat->asset_id), array('class'=>'select2', 'style'=>'min-width:600px')) }}
-                    {{ $errors->first('asset_id', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
-                </div>
-            </div>
+				<div class="col-md-10">
+					{{ Form::select('asset_id', $asset_list , Input::old('asset_id', $fixtureseat->asset_id), array('class'=>'select2', 'style'=>'min-width:600px')) }}
+					{{ $errors->first('asset_id', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
+				</div>
+			</div>
 
-
-            <!-- User -->
-            <div class="form-group {{ $errors->has('assigned_to') ? ' has-error' : '' }}">
-                <label for="assigned_to" class="col-md-2 control-label">@lang('admin/hardware/form.checkout_to')
+			<!-- User -->
+			<div class="form-group {{ $errors->has('assigned_to') ? ' has-error' : '' }}">
+				<label for="assigned_to" class="col-md-2 control-label">@lang('admin/hardware/form.checkout_to')
 				</label>
 
-                <div class="col-md-9">
-                    {{ Form::select('assigned_to', $users_list , Input::old('assigned_to', $fixtureseat->assigned_to), array('class'=>'select2', 'style'=>'min-width:350px')) }}
-                    {{ $errors->first('assigned_to', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
+				<div class="col-md-9">
+					{{ Form::select('assigned_to', $users_list , Input::old('assigned_to', $fixtureseat->assigned_to), array('class'=>'select2', 'style'=>'min-width:350px')) }}
+					{{ $errors->first('assigned_to', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
 
-                    <p class="help-block">
-                    @lang('admin/fixtures/form.checkout_help')
-                    </p>
+					<p class="help-block">
+					@lang('admin/fixtures/form.checkout_help')
+					</p>
 
-                </div>
-            </div>
+				</div>
+			</div>
 
+			<!-- Note -->
+			<div class="form-group {{ $errors->has('note') ? 'error' : '' }}">
+				<label for="note" class="col-md-2 control-label">@lang('admin/hardware/form.notes')</label>
+				<div class="col-md-7">
+					<textarea class="col-md-6 form-control" id="note" name="note">{{{ Input::old('note', $fixtureseat->note) }}}</textarea>
+					{{ $errors->first('note', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
+				</div>
+			</div>
 
-            <!-- Note -->
-            <div class="form-group {{ $errors->has('note') ? 'error' : '' }}">
-                <label for="note" class="col-md-2 control-label">@lang('admin/hardware/form.notes')</label>
-                <div class="col-md-7">
-                    <textarea class="col-md-6 form-control" id="note" name="note">{{{ Input::old('note', $fixtureseat->note) }}}</textarea>
-                    {{ $errors->first('note', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
-                </div>
-            </div>
+			<!-- Form actions -->
+			<div class="form-group">
+			<label class="col-md-2 control-label"></label>
+				<div class="col-md-7">
+					<a class="btn btn-link" href="{{ route('fixtures') }}">@lang('button.cancel')</a>
+					<button type="submit"  class="btn btn-success"><i class="fa fa-check icon-white"></i>  @lang('general.checkout')</button>
+				</div>
+			</div>
+		</form>
 
-
-            <!-- Form actions -->
-            <div class="form-group">
-            <label class="col-md-2 control-label"></label>
-                <div class="col-md-7">
-                    <a class="btn btn-link" href="{{ route('fixtures') }}">@lang('button.cancel')</a>
-                    <button type="submit"  class="btn btn-success"><i class="fa fa-check icon-white"></i>  @lang('general.checkout')</button>
-                </div>
-            </div>
-
-
-</form>
-
-</div>
+	</div>
 </div>
 
 @stop
