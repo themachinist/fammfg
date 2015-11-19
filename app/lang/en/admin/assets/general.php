ee4cc31557706aa0bc7181b13a6de866216bd2b4
David Winslow <the@machini.st>
2015-11-18 18:06:16 -0800

    added fixtures assigned entry

--- app/lang/en/admin/assets/general.php
+++ app/lang/en/admin/assets/general.php
@@ -10,6 +10,7 @@ return array(
     'deleted'  					=> 'This asset has been deleted. <a href="/hardware/:asset_id/restore">Click here to restore it</a>.',
     'edit'  					=> 'Edit Asset',
 	'filetype_info'				=> 'Allowed filetypes are png, gif, jpg, jpeg, doc, docx, pdf, txt, zip, and rar.',
+	'fixtures_assigned'			=> 'Fixtures Assigned', 
     'model_deleted'  			=> 'This Assets model has been deleted. You must restore the model before you can restore the Asset.<br/> <a href="/hardware/models/:model_id/restore">Click here to restore the model</a>.',
     'requestable'				=> 'Requestable',
     'restore'  					=> 'Restore Asset',
