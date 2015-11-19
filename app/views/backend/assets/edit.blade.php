cc03d7991bb07849d4ad2ceb8824c2579a2a1fac
David Winslow <the@machini.st>
2015-11-18 17:58:37 -0800

    subbed hardware for assets

--- app/views/backend/assets/edit.blade.php
+++ app/views/backend/assets/edit.blade.php
@@ -3,9 +3,9 @@
 {{-- Page title --}}
 @section('title')
     @if ($asset->id)
-    	@lang('admin/hardware/form.update') ::
+    	@lang('admin/assets/form.update') ::
     @else
-    	@lang('admin/hardware/form.create') ::
+    	@lang('admin/assets/form.create') ::
     @endif
 @parent
 @stop
@@ -100,9 +100,9 @@
             <a href="{{ URL::previous() }}" class="btn-flat gray pull-right right"><i class="fa fa-arrow-left icon-white"></i> @lang('general.back')</a>
         <h3>
         @if ($asset->id)
-        	@lang('admin/hardware/form.update')
+        	@lang('admin/assets/form.update')
         @else
-            @lang('admin/hardware/form.create')
+            @lang('admin/assets/form.create')
         @endif
         </h3>
     </div>
@@ -113,9 +113,9 @@
             <div class="col-md-12 column">
 
 			 @if ($asset->id)
-				 <form class="form-horizontal" method="post" action="{{ route('update/hardware',$asset->id) }}" autocomplete="off" role="form">
+				 <form class="form-horizontal" method="post" action="{{ route('update/assets',$asset->id) }}" autocomplete="off" role="form">
 			 @else
-				 <form class="form-horizontal" method="post" action="{{ route('savenew/hardware') }}" autocomplete="off" role="form">
+				 <form class="form-horizontal" method="post" autocomplete="off" role="form">
 			 @endif
 
             <!-- CSRF Token -->
@@ -123,7 +123,7 @@
 
             <!-- Asset Tag -->
             <div class="form-group {{ $errors->has('asset_tag') ? ' has-error' : '' }}">
-                <label for="asset_tag" class="col-md-2 control-label">@lang('admin/hardware/form.tag')
+                <label for="asset_tag" class="col-md-2 control-label">@lang('admin/assets/form.tag')
                  *</label>
                  </label>
                     <div class="col-md-7 col-sm-12">
@@ -139,7 +139,7 @@
 
             <!-- Serial -->
             <div class="form-group {{ $errors->has('serial') ? ' has-error' : '' }}">
-                <label for="serial" class="col-md-2 control-label">@lang('admin/hardware/form.serial') *</label>
+                <label for="serial" class="col-md-2 control-label">@lang('admin/assets/form.serial') *</label>
                 <div class="col-md-7 col-sm-12">
                     <input class="form-control" type="text" name="serial" id="serial" value="{{{ Input::old('serial', $asset->serial) }}}" />
                     {{ $errors->first('serial', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
@@ -148,7 +148,7 @@
 
             <!-- Asset Name -->
             <div class="form-group {{ $errors->has('name') ? ' has-error' : '' }}">
-                <label for="name" class="col-md-2 control-label">@lang('admin/hardware/form.name')</label>
+                <label for="name" class="col-md-2 control-label">@lang('admin/assets/form.name')</label>
                     <div class="col-md-7 col-sm-12">
                         <input class="form-control" type="text" name="name" id="name" value="{{{ Input::old('name', $asset->name) }}}" />
                         {{ $errors->first('name', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
@@ -157,13 +157,10 @@
 
             <!-- Model -->
             <div class="form-group {{ $errors->has('model_id') ? ' has-error' : '' }}">
-                <label for="parent" class="col-md-2 control-label">@lang('admin/hardware/form.model')
-                 *</label>
-                 </label>
+                <label for="parent" class="col-md-2 control-label">@lang('admin/assets/form.model') *</label>
                 <div class="col-md-7 col-sm-12">
                     @if (isset($selected_model))
                         {{ Form::select('model_id', $model_list , $selected_model->id, array('class'=>'select2 model', 'style'=>'min-width:400px','id' =>'model_select_id')) }}
-
                     @else
                         {{ Form::select('model_id', $model_list , Input::old('model_id', $asset->model_id), array('class'=>'select2 model', 'style'=>'min-width:400px','id' =>'model_select_id')) }}
                     @endif
@@ -176,7 +173,7 @@
 
             <!-- MAC Address -->
             <div id="mac_address" class="form-group {{ $errors->has('mac_address') ? ' has-error' : '' }}" style="display:none;">
-                <label for="mac_address" class="col-md-2 control-label">@lang('admin/hardware/form.mac_address')</label>
+                <label for="mac_address" class="col-md-2 control-label">@lang('admin/assets/form.mac_address')</label>
                     <div class="col-md-7 col-sm-12">
                         <input class="form-control" type="text" name="mac_address" id="mac_address" value="{{{ Input::old('mac_address', $asset->mac_address) }}}" />
                         {{ $errors->first('mac_address', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
@@ -185,7 +182,7 @@
 
             <!-- Purchase Date -->
             <div class="form-group {{ $errors->has('purchase_date') ? ' has-error' : '' }}">
-                <label for="purchase_date" class="col-md-2 control-label">@lang('admin/hardware/form.date')</label>
+                <label for="purchase_date" class="col-md-2 control-label">@lang('admin/assets/form.date')</label>
                 <div class="input-group col-md-3">
                     <input type="date" class="datepicker form-control" data-date-format="yyyy-mm-dd" placeholder="@lang('general.select_date')" name="purchase_date" id="purchase_date" value="{{{ Input::old('purchase_date', $asset->purchase_date) }}}">
                     <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
@@ -195,7 +192,7 @@
 
             <!-- Supplier -->
             <div class="form-group {{ $errors->has('supplier_id') ? ' has-error' : '' }}">
-                <label for="supplier_id" class="col-md-2 control-label">@lang('admin/hardware/form.supplier')</label>
+                <label for="supplier_id" class="col-md-2 control-label">@lang('admin/assets/form.supplier')</label>
                 <div class="col-md-7 col-sm-12">
                     {{ Form::select('supplier_id', $supplier_list , Input::old('supplier_id', $asset->supplier_id), array('class'=>'select2', 'style'=>'min-width:350px','id'=>'supplier_select_id')) }}
                     <a href='#' data-toggle="modal"  data-target="#createModal" data-dependency="supplier" data-select='supplier_select_id'><i class="verticon fa fa-plus-square-o fa-2x"></i></a>
@@ -205,7 +202,7 @@
 
             <!-- Order Number -->
             <div class="form-group {{ $errors->has('order_number') ? ' has-error' : '' }}">
-                <label for="order_number" class="col-md-2 control-label">@lang('admin/hardware/form.order')</label>
+                <label for="order_number" class="col-md-2 control-label">@lang('admin/assets/form.order')</label>
                 <div class="col-md-7 col-sm-12">
                     <input class="form-control" type="text" name="order_number" id="order_number" value="{{{ Input::old('order_number', $asset->order_number) }}}" />
                     {{ $errors->first('order_number', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
@@ -214,7 +211,7 @@
 
             <!-- Purchase Cost -->
             <div class="form-group {{ $errors->has('purchase_cost') ? ' has-error' : '' }}">
-                    <label for="purchase_cost" class="col-md-2 control-label">@lang('admin/hardware/form.cost') </label>
+                    <label for="purchase_cost" class="col-md-2 control-label">@lang('admin/assets/form.cost') </label>
                     <div class="col-md-2">
                             <div class="input-group">
                                     <span class="input-group-addon">
@@ -234,10 +231,10 @@
 
             <!-- Warranty -->
             <div class="form-group {{ $errors->has('warranty_months') ? ' has-error' : '' }}">
-                <label for="warranty_months" class="col-md-2 control-label">@lang('admin/hardware/form.warranty')</label>
+                <label for="warranty_months" class="col-md-2 control-label">@lang('admin/assets/form.warranty')</label>
                 <div class="col-md-2">
                     <div class="input-group">
-                    <input class="col-md-2 form-control" type="text" name="warranty_months" id="warranty_months" value="{{{ Input::old('warranty_months', $asset->warranty_months) }}}" />   <span class="input-group-addon">@lang('admin/hardware/form.months')</span>
+                    <input class="col-md-2 form-control" type="text" name="warranty_months" id="warranty_months" value="{{{ Input::old('warranty_months', $asset->warranty_months) }}}" />   <span class="input-group-addon">@lang('admin/assets/form.months')</span>
                     {{ $errors->first('warranty_months', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
                     </div>
                 </div>
@@ -245,13 +242,13 @@
 
             <!-- Status -->
             <div class="form-group {{ $errors->has('status_id') ? ' has-error' : '' }}">
-                <label for="status_id" class="col-md-2 control-label">@lang('admin/hardware/form.status') *</label>
+                <label for="status_id" class="col-md-2 control-label">@lang('admin/assets/form.status') *</label>
                     <div class="col-md-7 col-sm-12 col-sm-12">
                         {{ Form::select('status_id', $statuslabel_list , Input::old('status_id', $asset->status_id), array('class'=>'select2 status_id', 'style'=>'width:350px','id'=>'status_select_id')) }}
                         <a href='#' data-toggle="modal"  data-target="#createModal" data-dependency='statuslabel' data-select='status_select_id'><i class="verticon fa fa-plus-square-o fa-2x"></i></a>
                         <span class="status_spinner" style="padding-left: 10px; color: green; display:none; width: 30px;"><i class="fa fa-spinner fa-spin"></i> </span>
 
-                        <p class="help-block">@lang('admin/hardware/form.help_checkout')</p>
+                        <p class="help-block">@lang('admin/assets/form.help_checkout')</p>
                         {{ $errors->first('status_id', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
                     </div>
             </div>
@@ -259,7 +256,7 @@
             @if (!$asset->id)
              <!-- Assigned To -->
             <div id="assigned_user" style="display: none;" class="form-group {{ $errors->has('assigned_to') ? ' has-error' : '' }}">
-                <label for="parent" class="col-md-2 control-label">@lang('admin/hardware/form.checkout_to')
+                <label for="parent" class="col-md-2 control-label">@lang('admin/assets/form.checkout_to')
                  </label>
                 <div class="col-md-7 col-sm-12">
                     {{ Form::select('assigned_to', $assigned_to , Input::old('assigned_to', $asset->assigned_to), array('class'=>'select2', 'id'=>'assigned_to', 'style'=>'min-width:350px')) }}
@@ -271,7 +268,7 @@
 
             <!-- Notes -->
             <div class="form-group {{ $errors->has('notes') ? ' has-error' : '' }}">
-                <label for="notes" class="col-md-2 control-label">@lang('admin/hardware/form.notes')</label>
+                <label for="notes" class="col-md-2 control-label">@lang('admin/assets/form.notes')</label>
                 <div class="col-md-7 col-sm-12">
                     <textarea class="col-md-6 form-control" id="notes" name="notes">{{{ Input::old('notes', $asset->notes) }}}</textarea>
                     {{ $errors->first('notes', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
@@ -280,7 +277,7 @@
 
             <!-- Default Location -->
             <div class="form-group {{ $errors->has('rtd_location_id') ? ' has-error' : '' }}">
-                <label for="rtd_location_id" class="col-md-2 control-label">@lang('admin/hardware/form.default_location')</label>
+                <label for="rtd_location_id" class="col-md-2 control-label">@lang('admin/assets/form.default_location')</label>
                     <div class="col-md-7 col-sm-12">
                         {{ Form::select('rtd_location_id', $location_list , Input::old('rtd_location_id', $asset->rtd_location_id), array('class'=>'select2', 'style'=>'width:350px','id'=>'rtd_location_select')) }}
                          <a href='#' data-toggle="modal" data-target="#createModal" data-dependency='location' data-select='rtd_location_select'><i class="verticon fa fa-plus-square-o fa-2x"></i></a>
@@ -293,7 +290,7 @@
 				<div class="col-sm-offset-2 col-sm-10">
 				  <div class="checkbox">
 					<label>
-					  <input type="checkbox" value="1" name="requestable" id="requestable" {{ Input::old('requestable', $asset->requestable) == '1' ? ' checked="checked"' : '' }}> @lang('admin/hardware/form.requestable')
+					  <input type="checkbox" value="1" name="requestable" id="requestable" {{ Input::old('requestable', $asset->requestable) == '1' ? ' checked="checked"' : '' }}> @lang('admin/assets/form.requestable')
 					</label>
 				  </div>
 				</div>
